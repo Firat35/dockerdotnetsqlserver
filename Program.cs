0aa1a623262c8682fb2d3a6b541e86a90ccd2943
Firat35 <43121901+Firat35@users.noreply.github.com>
2021-04-05 21:32:53 +0300

    bugfix

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace BookManagement
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                {   webBuilder.UseUrls("http://*:80");
                     webBuilder.UseStartup<Startup>();
                 });
     }
